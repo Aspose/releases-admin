bf4366f852673dbfb862771a1818d5df11b04f12
Fahad Adeel <fahadadeel@gmail.com>
2022-05-11 14:14:14 +0500

    clean admin menu

--- resources/views/layouts/_admin_menu.blade.php
+++ resources/views/layouts/_admin_menu.blade.php
@@ -14,15 +14,15 @@
                         <li>
                             <a id="ctl00_lnkAddVenture" class="dropdown-toggle" href="{{ url('admin/ventures/amazon-s3-settings') }}">Amazon S3 Settings </a>
                         </li>
-                        <li>
+                        <!-- <li>
                             <a id="ctl00_lnkAddEngine" class="dropdown-toggle" href="ventures/manage-ui">UI Management</a>
                         </li>
                         <li>
                             <a id="ctl00_lnkAddRule" class="dropdown-toggle" href="ventures/manage-url-redirection">URL Redirection</a>
-                        </li>
+                        </li> -->
 
 
-                        <li class="nav-header"><i class="icon-wrench"></i>Product's Administration</li>
+                        <!-- <li class="nav-header"><i class="icon-wrench"></i>Product's Administration</li>
                         <li>
                             <a id="ctl00_lnkAddSection" class="dropdown-toggle" href="products/manage-families">Manage Product Families</a>
                         </li>
@@ -43,7 +43,7 @@
                         </li>
                         <li>
                             <a id="ctl00_HyperLink3" class="dropdown-toggle" href="seo-management/products">Product- SEO</a>
-                        </li>
+                        </li> -->
 
 
 
@@ -55,13 +55,13 @@
                             <a id="ctl00_HyperLink2" class="dropdown-toggle" href="file/manage-files">View All Files </a>
                         </li>
                         @if (Auth::user()->is_admin)
-                        <li class="nav-header"><i class="icon-wrench"></i>System Administration</li>
+                        <!-- <li class="nav-header"><i class="icon-wrench"></i>System Administration</li>
                         <li>
                             <a id="ctl00_HyperLink4" class="dropdown-toggle" href="administration/manage-roles">Manage Roles </a>
                         </li>
                         <li>
                             <a id="ctl00_HyperLink5" class="dropdown-toggle" href="administration/manage-users">Manage Users</a>
-                        </li>
+                        </li> -->
                         @endif
                     </ul>
 </div>
